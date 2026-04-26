f6d7d5b16257bf23b09ed84b27fd9d1e47d5ade4
Admin 01 <12040411307@students.uin-suska.ac.id>
2026-04-26 21:47:25 +0700

    change content file app/lib/mysql.ts

--- app/lib/mysql.ts
+++ app/lib/mysql.ts
@@ -1,14 +1,16 @@
-import mysql from 'mysql2/promise';
+import mysql, { PoolOptions } from 'mysql2/promise';
 
-export const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const access: PoolOptions = {
+  host: process.env.DB_HOST as string,
+  user: process.env.DB_USER as string,
+  password: process.env.DB_PASSWORD as string,
+  database: process.env.DB_NAME as string,
   port: 3306,
-  // UBAH BAGIAN INI:
-  ssl: false, // Mematikan SSL secara eksplisit
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-});
+};
+
+// Jangan masukkan properti ssl jika nilainya false
+// Ini cara paling aman agar lolos type check
+export const connection = mysql.createPool(access);
